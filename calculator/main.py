497626f7e84805d3a0538863d2498200cbaa2f08
Ethan <ethantoivo@gmail.com>
2025-12-09 00:08:17 -0500

    added error handling loops

--- calculator/main.py
+++ calculator/main.py
@@ -14,14 +14,36 @@ func_dict = {
 
 # Entry point for calc app
 if __name__ == "__main__":
-    user_input = int(input("input a number" + "\n"))
-    user_input2 = int(input("input another number" + "\n"))
+    # error handling loop
+    while True:
+        user_input = input("input a number" + "\n")
+        try:
+            user_input = int(user_input)
+        except:
+            print("must be an integer")
+            continue
 
-    # Ask the user if they want to add subtract mult or div
-    user_input_func = input("do you want to add sub mult or div" + "\n")
+        user_input2 = input("input another number" + "\n")
+        try:
+            user_input2 = int(user_input2)
+        except:
+            print("must be an integer")
+            continue
 
-    out = func_dict[user_input_func](user_input, user_input2)
+        # Ask the user if they want to add subtract mult or div
+        user_input_func = ""
+        while True:
+            user_input_func = input("do you want to add sub mult or div" + "\n")
+            if not user_input_func in list(func_dict.keys()):
+                print("must type \"add\" \"sub\" \"mult\" \"div\"")
+                continue
+            else:
+                break
+
+
+        out = func_dict[user_input_func](user_input, user_input2)
 
 
 
-    print(out)
+        print(out)
+        break
