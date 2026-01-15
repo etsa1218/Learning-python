7c9b6d524d1db08dd5152aa6590bafae630d98d9
Ethan <ethantoivo@gmail.com>
2026-01-15 18:09:16 -0500

    concatenated 11 into one file with comments separating, put 3 and 4 into file as well

---
+++ Numerical-Analysis-University/Homework-1/3.py
@@ -0,0 +1,31 @@
+import math
+
+# 3a
+p = 123
+q = 1106 / 9
+print(abs(p - q))
+
+# 3b
+p = 1 / math.e
+q = 0.3666
+print(abs(p - q))
+
+# 3c
+p = 2 ** 10
+q = 1000
+print(abs(p - q))
+
+# 3d
+p = 24
+q = 48
+print(abs(p - q))
+
+# 3e
+p = math.pi ** (-7)
+q = 10 ** -4
+print(abs(p - q))
+
+# 3f
+p = (0.062847) * (0.069234)
+q = 0.0042
+print(abs(p - q))
---
+++ Numerical-Analysis-University/Homework-1/4.py
@@ -0,0 +1,28 @@
+import math
+
+# 4a
+p = 123
+q = 1106 / 9
+print((abs(p - q)) / abs(p))
+
+# 4b
+p = 1 / math.e
+q = 0.3666
+print((abs(p - q)) / abs(p))
+
+# 4c
+p = 2 ** 10
+q = 1000
+print((abs(p - q)) / abs(p))
+
+# 4d
+p = 24
+q = 48
+print((abs(p - q)) / abs(p))
+
+# 4e
+p = math.pi ** (-7)
+q = 10 ** -4
+print((abs(p - q)) / abs(p))
+
+# 4f
